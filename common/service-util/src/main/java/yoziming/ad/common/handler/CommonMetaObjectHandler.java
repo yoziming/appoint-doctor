62d6a0866328f07b5936c5a4c35a3365bb53f971
yoziming <yoziming@gmail.com>
2022-01-06 03:42:22 +0800

    first

---
+++ common/service-util/src/main/java/yoziming/ad/common/handler/CommonMetaObjectHandler.java
@@ -0,0 +1,24 @@
+package yoziming.ad.common.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * Mybatis plus Handler配置类
+ */
+@Component
+public class CommonMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("createTime", new Date(), metaObject);
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+}
---
+++ service/service-hosp/src/main/java/yoziming/ad/servicehosp/ServiceHospApplication.java
@@ -0,0 +1,13 @@
+package yoziming.ad.servicehosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHospApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+
+}
